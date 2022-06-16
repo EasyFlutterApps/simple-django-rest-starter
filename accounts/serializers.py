f99817d642bc2ca9f19a1b356e017af90de4f296
Prashant Nigam <prashantnigam490@gmail.com>
2022-06-16 14:41:17 +0530

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,4 +7,4 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('id', 'username', 'email', 'first_name', 'last_name',
                   'date_of_birth', 'phone', 'photo')
-        extra_kwargs = {'password': {'write_only': True}}
+        depth = 1
